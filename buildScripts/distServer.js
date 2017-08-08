3e9c4a15f21b57fc032bf489dc667855f68e8a27
aragonhk <michael.c.ng@live.com>
2017-08-08 16:41:59 -0700

    commit

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -10,20 +10,17 @@ var app = express();
 import express from 'express';
 import path from 'path';
 import open from 'open';
-import webpack from 'webpack';
-import config from '../webpack.config.dev';
+import compression from 'compression';
+/* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
-const compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
-}));
+app.use(compression());
+app.use(express.static('dist'));
 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '../src/index.html'))
+    res.sendFile(path.join(__dirname, '../dist/index.html'))
 
 });
 
@@ -32,7 +29,7 @@ app.get('/users', function(req, res){
     res.json([
         {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
         {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tammy@gmail.com"},
-        {"id": 4,"firstName":"Tina","lastName":"Lee","email":"tina@gmail.com"},
+        {"id": 3,"firstName":"Tina","lastName":"Lee","email":"tina@gmail.com"},
     ]);
 });
 
